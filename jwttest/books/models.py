94c15ea34c31fb8853d957dcfa7d57b775bef415
mlhcc_1314@163.com <malong888>
2018-03-22 17:23:57 +0800

    jwt

--- jwttest/books/models.py
+++ jwttest/books/models.py
@@ -1,3 +1,8 @@
+"""
+model->序列化—>在settings中配置'rest_framework','app'->迁移数据库->编写视图函数views->编写url
+jwt api—auth生成token，相对于session的cookies。带token去请求主页面，就可以解析出来
+"""
+
 from django.db import models
 
 # Create your models here.
